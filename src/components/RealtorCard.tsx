0fceb123110f1b06296fef0ad892e4de5121feca
iamnafizahmed <iamnafizahmed@gmail.com>
2026-06-24 20:40:32 +0600

    Update RealtorCard.tsx

--- src/components/RealtorCard.tsx
+++ src/components/RealtorCard.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'motion/react';
-import noorLogo from '../assets/images/noor.png';
+import noorLogo from '../assets/images/noorw.png';
 import { 
   Phone, 
   Mail, 
